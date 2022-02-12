f8aff1be62f432d664cc24121b9c961889fb1ce2
JoJa <joeds133@hotmail.com>
2022-02-12 13:51:21 +0700

    edit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,41 +1,53 @@
-import React, {useState, useEffect} from 'react'
-import { FaDoorOpen, FaToilet } from "react-icons/fa" 
+import React, { useState, useEffect } from 'react'
+import { FaDoorOpen, FaToilet } from 'react-icons/fa'
 
 const Card = ({ data }) => {
-	const [elapsed, setElapsed] = useState(data.startTime)
+  const [elapsed, setElapsed] = useState(data.startTime)
 
-	useEffect(() => {
-		const intervalId = setInterval(() => {
-			setElapsed(elapsed + 1)
-		}, 1000)
-		return () => clearInterval(intervalId); 
-	}, [elapsed])
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setElapsed(elapsed + 1)
+    }, 1000)
+    return () => clearInterval(intervalId)
+  }, [elapsed])
 
-	return (
+  return (
     <div className="card">
-        <header>
-            <h1>ห้อง {data.id}</h1>
-        </header>
-        {data.isEmpty ? <FaDoorOpen name="toilet"/> : <FaToilet name="toilet"/>}
-        <p className={data.isEmpty ? "empty" : "used"}>
-            {data.isEmpty ? "ว่าง" : "ไม่ว่าง"}
-        </p>
-				{data.isEmpty ? '' : <div>
-					<p>เข้าไปตอน: {data.startTime}</p>
-					<p>เข้าไปแล้ว: {elapsed}</p>
-					<p>(น่า)จะว่างตอน</p>
-				</div>}
+      <header>
+        <h1>ห้อง {data.id}</h1>
+      </header>
+      {data.isEmpty ? <FaDoorOpen name="toilet" /> : <FaToilet name="toilet" />}
+
+      <p className={data.isEmpty ? 'empty' : 'used'}>
+        <font color="black" className="status">
+          สถานะ:
+        </font>
+        {data.isEmpty ? 'ว่าง' : 'ไม่ว่าง'}
+      </p>
+      {data.isEmpty ? (
+        ''
+      ) : (
+        <div className="word">
+          <p className="time-in">
+            เริ่มเข้าใช้งานตั้งแต่: {data.startTime} วินาที
+          </p>
+          <p className="time-out">เวลาที่ใช้: {elapsed} วินาที</p>
+          <p className="time-estimate">ใช้เวลาเสร็จสิ้นประมาณ: xxx วินาที</p>
+        </div>
+      )}
     </div>
   )
 }
 
 const Cards = ({ data }) => {
-    console.log(data)
-    return (
-        <div className="toilet-list">
-            {data.map(toilet => <Card key={toilet.id} data={toilet} />)}
-        </div>
-    )
+  console.log(data)
+  return (
+    <div className="toilet-list">
+      {data.map((toilet) => (
+        <Card key={toilet.id} data={toilet} />
+      ))}
+    </div>
+  )
 }
 
 export default Cards
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,33 @@
-import React from "react";
-import Cards from "../components/Card";
+import React from 'react'
+import Cards from '../components/Card'
 
 const Home = () => {
   const sample_toilet = [
     {
-      id: "1",
+      id: '1',
       isEmpty: true,
       startTime: 0,
     },
     {
-      id: "2",
+      id: '2',
       isEmpty: false,
       startTime: 0,
     },
-		{
-      id: "3",
+    {
+      id: '3',
       isEmpty: false,
       startTime: 0,
-    }
-  ];
+    },
+  ]
   return (
-    <div className="home">
-			<Cards data={sample_toilet} />
-			<p>รออีก ... นาที</p>
+    <div>
+      <h1 className="toilet-status">Toilet Status</h1>
+      <div className="home">
+        <Cards data={sample_toilet} />
+        <p className="estimate">รออีก ... นาที</p>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
